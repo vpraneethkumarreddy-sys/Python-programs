b2b14663504a132a8d631f5ddc45499964bd3d29
vpraneethkumarreddy-sys <vpraneethkumarreddy@gmail.com>
2026-05-26 09:21:57 +0530

    Create Numpy.py

---
+++ Numpy.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+arr1 = np.array([2,4])
+arr2 = np.array([1,3])
+
+print("Addition:", np.add(arr1, arr2))
+print("Subtraction:", np.subtract(arr1, arr2))
+print("Multiplication:", np.multiply(arr1, arr2))
+print("Sum:", np.sum(arr1))
+print("Mean:", np.mean(arr1))
+print("Ones Array:\n", np.ones((2,2)))
+print("Zeros Array:\n", np.zeros((2,2)))
